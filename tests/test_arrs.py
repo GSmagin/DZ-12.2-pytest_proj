cfa09fd10bf1df315531d819a792319d9a28b6c8
SmaginG <test@yandex.ru>
2024-02-26 02:18:58 +0300

    100% покрытия тестами

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,8 +5,11 @@ def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([1, 2, 3], 2, "test") == 3
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], -1, 2) == 2
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 0) == []
+    assert arrs.my_slice([1, 2, 3, 4], end=0) == []
